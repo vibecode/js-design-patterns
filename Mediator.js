9159f2fd3485f82d0c6feb1f2a0b22d539d8ccc6
vibecode <rumashkin@gmail.com>
2017-08-11 18:10:29 +0200

    add another mediator example

--- Mediator.js
+++ Mediator.js
@@ -6,11 +6,9 @@
 //Helps to pinpoint dependencies
 //decoupling objects which often promotes smaller, reusable components
 
-
 //CONS:
 //Introduces a single point of failure
 //When modules communicate back and forth using a mediator pattern, it tends to become cumbersome and usually results in a clear performance hit. It's best when the mediator is only used to coordinate actions across multiple features and not for communication within the individual features themselves; keep the airways clean! (via https://carldanley.com/js-mediator-pattern/)
-
 
 class AbstractHouse {
   constructor(greatLord) {
@@ -46,3 +44,34 @@ class HouseStark {
 
 const houseStark = new HouseStark();
 houseStark.houses.bolton.sendMessage('karstark', 'ty pidor');
+
+//https://stackoverflow.com/questions/25417547/observer-pattern-vs-mediator-pattern
+//photo upload
+
+class UploadMediator {
+  constructor() {
+    this.actors = {};
+  }
+
+  registerActor(name, obj) {
+    this.actors[name] = obj;
+  }
+
+  launch() {
+    this.actors['chooser'].show();
+    this.actors['preview'].hide();
+    this.actors['progress'].hide();
+  }
+
+  selected(img) {
+    this.actors['preview'].show(img);
+  }
+
+  uploading(progressNotifier) {
+    this.actors['progress'].show(progressNotifier)
+  }
+
+  uploaded(thumbUrl) {
+    //show thumbUrl in the image list
+  }
+}
